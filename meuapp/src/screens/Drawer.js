6d8c92e5512da512565d096164ba87c72d7e062a
Mello <61852906+gustavomello-source@users.noreply.github.com>
2024-11-15 15:25:02 -0300

    Update Drawer.js Style and Function

--- meuapp/src/screens/Drawer.js
+++ meuapp/src/screens/Drawer.js
@@ -1,14 +1,131 @@
-import { createDrawerNavigator } from "@react-navigation/drawer";
+import React from 'react';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { TouchableOpacity, Image, View, Text, StyleSheet } from 'react-native';
+import { DrawerContentScrollView, DrawerItem } from '@react-navigation/drawer';
+import Home from './Home';
 import AcoesPesquisa from './AcoesPesquisa';
 
 const DrawerNavigator = createDrawerNavigator();
 
-const Drawer = () => {
+const CustomDrawerContent = (props) => {
     return (
-        <DrawerNavigator.Navigator>
-            <DrawerNavigator.Screen name="AcoesPesquisa" component={AcoesPesquisa} />
-        </DrawerNavigator.Navigator>
+      <View style={styles.container}>
+        <DrawerContentScrollView {...props} style={styles.drawerContent}>
+          <View style={styles.emailContainer}>
+            <Text style={styles.emailText}>usuario@dominio.com</Text>
+            <View style={styles.divider} />
+          </View>
+  
+          <DrawerItem
+            label="Pesquisas"
+            labelStyle={styles.drawerItemLabel}
+            icon={({ color, size }) => (
+              <Image
+                source={require('../img/file_icon.png')}
+                style={styles.drawerIcon}
+              />
+            )}
+            //onPress={() => props.navigation.navigate('AcoesPesquisa')}
+          />
+        </DrawerContentScrollView>
+  
+        <View style={styles.logoutContainer}>
+          <DrawerItem
+            label="Sair"
+            labelStyle={styles.drawerItemLabel}
+            icon={({ color, size }) => (
+              <Image
+                source={require('../img/logout_icon.png')}
+                style={styles.drawerIcon}
+              />
+            )}
+            onPress={() => {
+              props.navigation.popToTop();
+            }}
+          />
+        </View>
+      </View>
     );
+  };
+
+const Drawer = () => {
+  return (
+    <DrawerNavigator.Navigator
+      drawerContent={(props) => <CustomDrawerContent {...props} />}
+      screenOptions={{
+        drawerStyle: {
+          backgroundColor: '#2B1D62',
+          width: 240,
+        },
+        headerStyle: {
+          backgroundColor: '#2B1D62',
+        },
+        headerTintColor: '#573FBA',
+      }}
+    >
+      <DrawerNavigator.Screen 
+        name="Home" 
+        component={Home} 
+        options={({ navigation }) => ({
+          headerTitleStyle: {
+            fontFamily: 'AveriaLibre-Regular',
+            fontWeight: '400',
+            color: '#FFFFFF',
+            fontSize: 38,
+          },
+          title: '',
+          headerLeft: () => (
+            <TouchableOpacity
+              onPress={() => navigation.toggleDrawer()}>
+              <Image
+                style={{ width: 50, height: 26, marginLeft: 15 }}
+                source={require('../img/home_icone.png')}
+              />
+            </TouchableOpacity>
+          ),
+        })}
+      />
+    </DrawerNavigator.Navigator>
+  );
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#2B1D62',
+    },
+    drawerContent: {
+      flex: 1,
+      backgroundColor: '#2B1D62',
+    },
+    emailContainer: {
+      padding: 16,
+      marginTop: 20,
+    },
+    emailText: {
+      color: '#FFFFFF',
+      fontSize: 16,
+      fontFamily: 'AveriaLibre-Regular',
+      marginBottom: 10,
+    },
+    divider: {
+      height: 1,
+      backgroundColor: '#FFFFFF',
+      marginTop: 5,
+    },
+    drawerItemLabel: {
+      color: '#FFFFFF',
+      fontSize: 16,
+      fontFamily: 'AveriaLibre-Regular',
+    },
+    drawerIcon: {
+      width: 24,
+      height: 24,
+      resizeMode: 'contain'
+    },
+    logoutContainer: {
+      paddingBottom: 20,
+    },
+  });
 
 export default Drawer;
