5e5cb67c458f16c60b79939060a2d6cb001cb740
Thaisse Kirian <thakirian@gmail.com>
2024-11-18 22:09:14 -0300

    Update Icons

--- meuapp/src/screens/Coleta.js
+++ meuapp/src/screens/Coleta.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {View, Text, Image, TouchableOpacity, StyleSheet} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const Coleta = props => {
 
@@ -12,27 +13,32 @@ const Coleta = props => {
         <Text style={styles.text}>O que você achou do Carnaval 2024?</Text>
         <View style={styles.div}>
             <TouchableOpacity style={styles.button} onPress={AgradecimentoPartic}>
-            <Image style={styles.img} source={require('../img/Coleta01.png')} />
+            {/* <Image style={styles.img} source={require('../img/Coleta01.png')} /> */}
+            <Icon name="emoticon-frown-outline" size={70} color="red" />
             <Text style={styles.textFig}>Péssimo </Text>
             </TouchableOpacity>
 
             <TouchableOpacity style={styles.button} onPress={AgradecimentoPartic}>
-            <Image style={styles.img} source={require('../img/Coleta02.png')} />
+            {/* <Image style={styles.img} source={require('../img/Coleta02.png')} /> */}
+            <Icon name="emoticon-sad-outline" size={70} color="orange" />
             <Text style={styles.textFig}>Ruim</Text>
             </TouchableOpacity>
 
             <TouchableOpacity style={styles.button} onPress={AgradecimentoPartic}>
-            <Image style={styles.img} source={require('../img/Coleta03.png')} />
+            {/* <Image style={styles.img} source={require('../img/Coleta03.png')} /> */}
+            <Icon name="emoticon-neutral-outline" size={70} color="yellow" />
             <Text style={styles.textFig}>Neutro </Text>
             </TouchableOpacity>
 
             <TouchableOpacity style={styles.button} onPress={AgradecimentoPartic}>
-            <Image style={styles.img} source={require('../img/Coleta04.png')} />
+            {/* <Image style={styles.img} source={require('../img/Coleta04.png')} /> */}
+            <Icon name="emoticon-happy-outline" size={70} color="cyan" />
             <Text style={styles.textFig}>Bom </Text>
             </TouchableOpacity>
 
             <TouchableOpacity style={styles.button} onPress={AgradecimentoPartic}>
-            <Image style={styles.img} source={require('../img/Coleta05.png')} />
+            {/* <Image style={styles.img} source={require('../img/Coleta05.png')} /> */}
+            <Icon name="emoticon-excited-outline" size={70} color="green" />
             <Text style={styles.textFig}>Excelente </Text>
             </TouchableOpacity>
         </View>
@@ -65,10 +71,6 @@ const styles = StyleSheet.create({
   button: {
     justifyContent: 'center',
     alignItems: 'center',
-  },
-  img: {
-    width: '65',
-    height: '65',
   },
   textFig: {
     fontFamily: 'AveriaLibre-Regular',
