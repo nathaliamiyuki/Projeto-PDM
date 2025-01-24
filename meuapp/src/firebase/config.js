97797aa7ace96c26d96fc309efe6e20358eee453
Mello <61852906+gustavomello-source@users.noreply.github.com>
2025-01-24 17:15:57 -0300

    Update config.js
    Atualiza as chaves de API do config do firebase

--- meuapp/src/firebase/config.js
+++ meuapp/src/firebase/config.js
@@ -1,14 +1,13 @@
-
 import { initializeApp } from "firebase/app";
 import { getAuth} from "firebase/auth"
 
 const firebaseConfig = {
-  apiKey:  "AIzaSyB7D1gZ0AUJgSBlTl15949hFKVywOzhVsA",
-  authDomain: "projeto-pdm-es47a.firebaseapp.com",
-  projectId:  "projeto-pdm-es47a",
-  storageBucket: "projeto-pdm-es47a.firebasestorage.app",
-  messagingSenderId: "514691912402",
-  appId:   "1:514691912402:web:abe137781ed0e9fbe8130b"
+  apiKey: "AIzaSyAJG4SlehPBIWSY_Hd-uvmmII-B8Yyjj_s",
+  authDomain: "projeto-pdm-firebase.firebaseapp.com",
+  projectId: "projeto-pdm-firebase",
+  storageBucket: "projeto-pdm-firebase.firebasestorage.app",
+  messagingSenderId: "193996804367",
+  appId: "1:193996804367:web:098340fcb92e0990e4ae7a"
 };
 
 // Initialize Firebase
